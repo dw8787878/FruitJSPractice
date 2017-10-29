76baa98121e97ce863f7a475219aab7f975e5578
Darryn Wu <darryn8wu@gmail.com>
2017-10-29 12:59:01 -0400

    added fruitFilter(), and started main()

--- main.js
+++ main.js
@@ -114,6 +114,18 @@ class fruitByMonthData {
   }
 }
 
+//fruitFilter, example: eachMonthData = 'allPricesByMonth', criteria = [Month Name], criteraValue = [Month Name]
+function fruitFilter(eachMonthData, criteria, criteriaValue){
+
+  let filtered = eachMonthData.filter(function (pricePoint){
+    //   console.log('criteria: ', criteria)
+    //   console.log('pricePoint: ', pricePoint)
+        return pricePoint[criteria] === criteriaValue
+  })
+  return filtered
+}
+
+/*
   // takes in pricing data for that month
   // returns an object by sorted by fruit type
 function sortByFruits(eachMonthData){
@@ -122,7 +134,7 @@ function sortByFruits(eachMonthData){
 //   console.log(eachMonthData)
 
   // this array stores the average price of each fruit for that month
-  let fruitPricesMonth = []
+  let fruitPricesMonth =
   // initialize with first price point from eachMonthData
   fruitPricesMonth.push(new fruitByMonthData(eachMonthData[0].Name, eachMonthData[0].Region, eachMonthData[0].Price))
   eachMonthData.splice(0,1)
@@ -142,6 +154,69 @@ function sortByFruits(eachMonthData){
   })
 }
 
+*/
+    var monthlyFruitRegionPrices = {}
 
+function main() {
 
-sortByFruits(allPricesByMonth.January)
+    let avgPrice = 0
+console.log('WHOO HOO!')
+    for (month in allPricesByMonth){
+      for (let i = 0; i < possibleFruits.length; i++){
+        //this is going to give us, the fruit in that month.
+        let fruitName = possibleFruits[i].FruitName
+        let fruit = fruitFilter([month], 'Name', fruitName)
+        //we now need to filter by region...
+        for (let i = 0; i < possibleRegions.length; i++) {
+          let regionName = possibleRegions[i].RegionName
+          let fruitByRegion = fruitFilter(fruit, 'Region', regionName)
+          for (let i = 0; i < fruitByRegion; i++){
+              avgPrice += fruitByRegion[i].price
+              avgPrice = avgPrice / 2
+              console.log('here')
+          }
+              let variableName = `${month+fruitName+regionName}`
+              monthlyFruitRegionPrices[variableName] = avgPrice
+        }
+      }
+    }
+}
+
+/*
+  let temp = {}
+  let temp2 = {}
+  let avgPrice = 0
+
+  for (month in allPricesByMonth){
+    temp[month] = []
+    for (let i = 0; i < possibleFruits.length; i++){
+      //create an array for each fruit type, inside per month
+      temp[month].push([])
+      temp[month][i] =
+    //   temp[month][i].push()
+    var fruitName = possibleFruits[i].FruitName
+    var fruit = fruitFilter(allPricesByMonth[month], 'Name', fruitName)
+      for (let i = 0; i < possibleRegions.length; i++) {
+          var regionName = possibleRegions[i].RegionName
+         var fruitRegion = fruitFilter(fruit, 'RegionName', regionName)
+      }
+        for(let i = 0; i < fruitRegion.length; i++){
+            avgPrice += fruitRegion[i].Price
+            avgPrice = avgPrice / 2
+        }
+    }
+        let variableName = `${month+fruitName+regionName}`
+        temp2[variableName] = avgPrice
+  }
+ console.log('temp2:', temp2)
+}
+*/
+
+// let test = fruitFilter('January', 'Name', possibleFruits.FruitName)
+// console.log(test)
+
+main()
+console.log('monthlyFruitRegionPrices: ', monthlyFruitRegionPrices)
+
+// console.log('perFruitMonth:', perFruitMonth)
+//console.log('janApplePrices:', janApplePrices)
